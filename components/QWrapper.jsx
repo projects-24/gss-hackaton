efc18dd6916bd5ed3285b19d4d30fcc62589f128
iddrismystic <funtechs45@gmail.com>
2024-08-09 10:21:33 +0000

    i

--- components/QWrapper.jsx
+++ components/QWrapper.jsx
@@ -1,7 +1,14 @@
+import TextUi from '@/ui/Text'
 import React from 'react'
 
-export default function QWrapper({children}) {
+export default function QWrapper({q, children}) {
   return (
-    <div className="padding-20 round-edge border margin-bottom-20">{children}</div>
+    <div className="padding-20 round-edge border margin-bottom-20">
+      <div className="margin-botto-40">
+        <TextUi text={q} size='big' color='dark500' />
+      </div>
+      {children}
+    </div>
+
   )
 }
